e879f44a26255f6b01260168ccb74f79afe8d117
keyen_eye <david02ing@gmail.com>
2021-04-26 22:13:48 -0500

    [add] react helmet

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react'
+import { Helmet } from 'react-helmet';
 import { Link } from 'react-router-dom';
 import AppContext from '../context/AppContext'
 import '../styles/components/Checkout.css'
@@ -18,31 +19,36 @@ const Checkout = () => {
     }
 
     return (
-        <div className="Checkout">
-            <div className="Checkout-content">
-                <h3>Lista de pedidos</h3>
-                {cart.length > 0 ? <h3>Lista de pedidos:</h3> : <h3>Sin pedidos...</h3>}
-                {cart.map((item) => (
-                    <div key={item.id} className="Checkout-item">
-                        <div className="Checkout-element">
-                            <h4>{item.title}</h4>
-                            <span>${item.price}</span>                        
+        <React.Fragment>
+            <Helmet>
+                <title>Lista de Pedidos - Platzi Conf Merch</title>
+            </Helmet>
+            <div className="Checkout">
+                <div className="Checkout-content">
+                    <h3>Lista de pedidos</h3>
+                    {cart.length > 0 ? <h3>Lista de pedidos:</h3> : <h3>Sin pedidos...</h3>}
+                    {cart.map((item) => (
+                        <div key={item.id} className="Checkout-item">
+                            <div className="Checkout-element">
+                                <h4>{item.title}</h4>
+                                <span>${item.price}</span>                        
+                            </div>
+                            <button type="button" onClick={handleRemove(item)}>
+                                <i className="fas fa-trash-alt" />
+                            </button>
                         </div>
-                        <button type="button" onClick={handleRemove(item)}>
-                            <i className="fas fa-trash-alt" />
-                        </button>
-                    </div>
-                ) )}
-            </div>
-            {cart.length > 0 && (
-                <div className="Checkout-sidebar">
-                    <h3>{`Precio total: $ ${handleSumTotal()}`}</h3>
-                    <Link to="/checkout/information">
-                        <button type="button">Continuar pedido</button>
-                    </Link>
+                    ) )}
                 </div>
-            )}
-        </div>
+                {cart.length > 0 && (
+                    <div className="Checkout-sidebar">
+                        <h3>{`Precio total: $ ${handleSumTotal()}`}</h3>
+                        <Link to="/checkout/information">
+                            <button type="button">Continuar pedido</button>
+                        </Link>
+                    </div>
+                )}
+            </div>
+        </React.Fragment>
     )
 }
 
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,12 +1,36 @@
 import React from 'react'
+import { Helmet } from 'react-helmet';
 import Products from '../components/Products';
 import initialState from '../initialState';
 
 const Home = () => {
     return (
-        <div>
+        <React.Fragment>
+            <Helmet>
+                <meta name="twitter:card" content="summary_large_image"/>
+                <meta name="twitter:site" content="@EyeKeyen"/>
+                <meta name="twitter:creator" content="@EyeKeyen"/>
+                <meta name="twitter:title" content="Platzi Conf Store"/>
+                <meta name="twitter:description" content="Platzi Conf Store"/>
+                <meta
+                    name="twitter:image"
+                    content="https://pbs.twimg.com/profile_images/1082796929384415235/N1YCFrXI_400x400.jpg"
+                />
+                <meta property="og:title" content="Platzi Conf Store"/>
+                <meta property="og:description" content="Platzi Conf Store"/>
+                <meta
+                    property="og:image"
+                    content="https://pbs.twimg.com/profile_images/1082796929384415235/N1YCFrXI_400x400.jpg"
+                />
+                <meta property="og:url" content="platzistore.xyz" />
+                <meta property="og:site_name" content="Platzi Conf Store" />
+                <meta property="og:locale" content="es_ES" />
+                <meta property="og:type" content="article" />
+                <meta property="fb:app_id" content="ID_APP_FACEBOOK" />
+                <title>Platzi Conf Merch - Productos</title>
+            </Helmet>
             <Products products={initialState.products} />
-        </div>
+        </React.Fragment>
     )
 }
 
